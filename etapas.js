d512910372523f27429f3ed21468a69dfb2590cb
VinicinBraga <vinicius@bragamatta.com.br>
2021-08-11 16:46:05 -0300

    Projeto Urna

---
+++ etapas.js
@@ -0,0 +1,99 @@
+let etapas = [
+  {
+    titulo: 'PRESIDENTE',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: '30',
+        nome: 'João Amoedo',
+        vice: 'Christian Lohbauer',
+        partido: 'Novo',
+        fotos: [
+          { url: 'JoaoAmoedo.jpg', legenda: 'Presidente', small: false },
+          { url: 'ChristianLohbauer.jpg', legenda: 'Vice-Presidente', small: true}
+        ]
+      },
+      {
+        numero: '17',
+        nome: 'Jair Bolsonaro',
+        vice: 'General Mourao',
+        partido: 'PSL',
+        fotos: [
+          { url: 'bolsonaro.jpg', legenda: 'Presidente', small: false },
+          { url: 'GeneralMourao.jpeg', legenda: 'Vice-Presidente', small: true}
+        ]
+      },
+      {
+        numero: '13',
+        nome: 'Haddad',
+        vice: 'Manuela Davila',
+        partido: 'PT',
+        fotos: [
+          { url: 'Haddad.jpeg', legenda: 'Presidente', small: false },
+          { url: 'ManuelaD.jpeg', legenda: 'Vice-Presidente', small: true}
+        ]
+      },
+      {
+        numero: '18',
+        nome: 'Marina Silva',
+        vice: 'Eduardo Jorge',
+        partido: 'Rede e PV',
+        fotos: [
+          { url: 'MarinaSilva.jpeg', legenda: 'Presidente', small: false },
+          { url: 'EduardoJorge.jpeg', legenda: 'Vice-Presidente', small: true}
+        ]
+      },
+      {
+        numero: '50',
+        nome: 'Guilhrme Boulos',
+        vice: 'Sonia Guajajara',
+        partido: 'PSOL',
+        fotos: [
+          { url: 'GuilhermeBoulos.jpg', legenda: 'Presidente', small: false },
+          { url: 'Sonia.jpeg', legenda: 'Vice-Presidente', small: true}
+        ]
+      },
+      {
+        numero: '51',
+        nome: 'Cabo Daciolo',
+        vice: 'Suelene Balduino',
+        partido: 'Patriota',
+        fotos: [
+          { url: 'CaboDaciolo.jpeg', legenda: 'Presidente', small: false },
+          { url: 'Suelene Balduino.jpeg', legenda: 'Vice-Presidente', small: true}
+        ]
+      }
+    ]
+  },
+  {
+    titulo: 'SENADOR',
+    numeros: 2,
+    candidatos: [
+      ,
+      {
+        numero: '10',
+        nome: 'Mussum',
+        partido: 'MÉ',
+        fotos: [
+          { url: 'Mussum.jpeg', legenda: 'Senador', small: false }
+        ]
+      },
+      {
+        numero: '07',
+        nome: 'CR7',
+        partido: 'JUV',
+        fotos: [
+          { url: 'CR7.jpeg', legenda: 'Senador', small: false }
+        ]
+      },
+      {
+        numero: '18',
+        nome: 'Silvio Santos',
+        partido: 'SBT',
+        fotos: [
+          { url: 'Silvio.jpeg', legenda: 'Senador', small: false }
+        ]
+      }
+    ]
+  },
+]
